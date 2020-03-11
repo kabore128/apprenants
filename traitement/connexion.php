cfe1e75746e7a767aedf2749c573c3dba2f02a2d
florida <elsifloridao@gmail.com>
2020-03-11 15:45:03 +0000

    modification encore

--- traitement/connexion.php
+++ traitement/connexion.php
@@ -1,8 +1,8 @@
-<?php $bdd = new PDO('mysql:host=localhost;dbname=apprenants', 'root', ''); 
+<?php  
 try 
 {
      $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION; 
-     $bdd = new PDO('mysql:host=localhost;dbname=test', 'root', '',
+     $bdd = new PDO('mysql:host=localhost;dbname=apprenants', 'root', '',
       $pdo_options); } 
       catch (Exception $e)
        {
